2d0eedc5ac8f5cb0d833f7f00189f04c917f71cc
Harry Patrick <harry.g.patirck94@gmail.com>
2020-01-29 15:35:27 +1100

    added home and search component

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import SearchFlight from './SearchFlight'
+
+
+class Home extends React.Component {
+
+  render(){
+
+    return(
+      <div>
+        <hr/>
+        <SearchFlight />
+        <hr/>
+      </div>
+
+    ) // return
+  } // render
+
+}
+
+export default Home
---
+++ src/components/SearchFlight.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class SearchFlight extends React.Component {
+
+
+  render(){
+
+    return(
+      <h1>SearchFlight here</h1>
+    ) // return
+  } // render
+} // SearchFlight
+
+export default SearchFlight
